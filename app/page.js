2440790456e209497513aac73f99dbf9bd2ba4ed
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2024-11-23 21:42:33 +0530

    some stuff

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Homenav from "./components/Homenav";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Homenav />
+    </>
+  );
 }
