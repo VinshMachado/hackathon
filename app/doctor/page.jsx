e615bd466addcddca91319231f65a739dceb0a87
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-01-11 22:01:48 +0530

    Update page.jsx

--- app/doctor/page.jsx
+++ app/doctor/page.jsx
@@ -7,7 +7,7 @@ const videocall = () => {
         <h2 className="pl-20 text-xl font-semibold">Call</h2>
       </div>
       <iframe
-        src="https://vinishmachado.whereby.com/messiisthegoat0d9ae424-596c-4f4b-8ca7-25a6a37badac?roomKey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZWV0aW5nSWQiOiI5NTI1ODEwMiIsInJvb21SZWZlcmVuY2UiOnsicm9vbU5hbWUiOiIvbWVzc2lpc3RoZWdvYXQwZDlhZTQyNC01OTZjLTRmNGItOGNhNy0yNWE2YTM3YmFkYWMiLCJvcmdhbml6YXRpb25JZCI6IjMwMzgzNiJ9LCJpc3MiOiJodHRwczovL2FjY291bnRzLnNydi53aGVyZWJ5LmNvbSIsImlhdCI6MTczNjQzNDM0Nywicm9vbUtleVR5cGUiOiJtZWV0aW5nSG9zdCJ9.o__8l_dh5ayu9E1pRQWFpQYW7DyS5XghRzid96Gy4O8"
+        src="https://vinishmachado.whereby.com/doconcall130fedf3-316c-4995-8a41-6be7ea4d7b2c?roomKey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZWV0aW5nSWQiOiI5NTM0MjczMiIsInJvb21SZWZlcmVuY2UiOnsicm9vbU5hbWUiOiIvZG9jb25jYWxsMTMwZmVkZjMtMzE2Yy00OTk1LThhNDEtNmJlN2VhNGQ3YjJjIiwib3JnYW5pemF0aW9uSWQiOiIzMDM4MzYifSwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50cy5zcnYud2hlcmVieS5jb20iLCJpYXQiOjE3MzY2MTMwMDcsInJvb21LZXlUeXBlIjoibWVldGluZ0hvc3QifQ.uci4-z17LqzPH_cfy8GHAkKiMZmDqwyD8o_wkQAPI3g"
         allow="camera; microphone; fullscreen"
         className="w-full h-[600px]"
       ></iframe>
